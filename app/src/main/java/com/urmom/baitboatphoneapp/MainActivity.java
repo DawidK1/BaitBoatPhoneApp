d6c42386ea1fd34a4f13d081e5494977a64b9057
DawidK1 <you@example.com>
2023-03-23 18:15:40 +0100

    sending move command and receiving and parsing data, no GUI yet

--- app/src/main/java/com/urmom/baitboatphoneapp/MainActivity.java
+++ app/src/main/java/com/urmom/baitboatphoneapp/MainActivity.java
@@ -1,22 +1,58 @@
 package com.urmom.baitboatphoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.zerokol.views.joystickView.JoystickView;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+    private Button mTestButton = null;
+    private JoystickView joystickView = null;
+
+    private  protocolParser mParser = null;
+    private UartService mService = null;
+    Handler mHandler;
+
+    private int mJoyPower = 0;
+    private int mJoyAngle = 0;
+
+
+    private final static String TAG = "BaitBoatPhoneApp";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String TAG = "MainActivityBoat";
 
-
+        mHandler = new Handler();
 
         // Make sure we have access coarse location enabled, if not, prompt the user to enable it
         if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -31,6 +67,218 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
             builder.show();
+
+        }
+        mTestButton = findViewById(R.id.button);
+        joystickView = findViewById(R.id.joystickView);
+
+
+        mTestButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "Button pressed");
+                StartConnecting();
+            }
+        });
+
+        joystickView.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
+            @Override
+            public void onValueChanged(int angle, int power, int direction) {
+                onJoystickUpdate(power, angle);
+            }
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);
+
+        mParser = new protocolParser();
+        service_init();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                periodicControlSend();
+                mHandler.postDelayed(this, 200);
+            }
+        }, 2000);
+
+        Log.d(TAG, "OnCreate done");
+    }
+
+
+    private final BroadcastReceiver UARTStatusChangeReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            final Intent mIntent = intent;
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_CONNECTED)) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+//                        String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+//                        Log.d(TAG, "UART_CONNECT_MSG");
+//                        btnConnectDisconnect.setText("Disconnect");
+//                        uartConnected = true;
+//                        ((TextView) findViewById(R.id.deviceName)).setText(mDevice.getName()+ " - ready");
+//                        listAdapter.add("["+currentDateTimeString+"] Connected to: "+ mDevice.getName());
+//                        messageListView.smoothScrollToPosition(listAdapter.getCount() - 1);
+//                        mState = UART_PROFILE_CONNECTED;
+                    }
+                });
+            }
+
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_DISCONNECTED)) {
+                runOnUiThread(new Runnable() {
+                    public void run() {
+//                        String currentDateTimeString = DateFormat.getTimeInstance().format(new Date());
+//                        Log.d(TAG, "UART_DISCONNECT_MSG");
+//                        btnConnectDisconnect.setText("Connect");
+//                        uartConnected = false;
+//                        ((TextView) findViewById(R.id.deviceName)).setText("Not Connected");
+//                        listAdapter.add("["+currentDateTimeString+"] Disconnected to: "+ mDevice.getName());
+//                        mState = UART_PROFILE_DISCONNECTED;
+                        mService.close();
+                        //setUiState();
+
+                    }
+                });
+            }
+
+
+            //*********************//
+            if (action.equals(UartService.ACTION_GATT_SERVICES_DISCOVERED)) {
+                mService.enableTXNotification();
+            }
+            //*********************//
+            if (action.equals(UartService.ACTION_DATA_AVAILABLE)) {
+                while (mService.hasRxBytes()) {
+                    mParser.addByte(mService.getRxByte());
+                }
+                processBoatMessages();
+            }
+            //*********************//
+            if (action.equals(UartService.DEVICE_DOES_NOT_SUPPORT_UART)) {
+//                showMessage("Device doesn't support UART. Disconnecting");
+                mService.disconnect();
+            }
+            //*********************//
+            if (action.equals(UartService.DEVICE_DOES_NOT_SUPPORT_UART)) {
+//                showMessage("Device doesn't support UART. Disconnecting");
+                mService.disconnect();
+            }
+
+        }
+    };
+
+    //UART service connected/disconnected
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder rawBinder) {
+            mService = ((UartService.LocalBinder) rawBinder).getService();
+            Log.d(TAG, "onServiceConnected mService= " + mService);
+            if (!mService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            Log.d(TAG, "onServiceDisconnected mService= ");
+        }
+
+    };
+
+    private void service_init() {
+        Intent bindIntent = new Intent(this, UartService.class);
+        bindService(bindIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(UARTStatusChangeReceiver, makeGattUpdateIntentFilter());
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(UartService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(UartService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(UartService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(UartService.DEVICE_DOES_NOT_SUPPORT_UART);
+        return intentFilter;
+    }
+
+    void StartConnecting() {
+        if (mService.isTxPossible()) {
+            sendLoadDrop();
+        } else {
+
+            mService.initialize();
+            mService.startScanning();
+        }
+
+    }
+
+    void ShowWarning(String warningText)
+    {
+
+    }
+
+    void processBoatMessages()
+    {
+        protocolParser.Command cmd;
+
+        mParser.parseRxBuffer();
+
+        while (mParser.isRxCommandAvailable())
+        {
+            cmd = mParser.getBoatMessage();
+            Log.d(TAG,"Got Boat msg! ID is " + cmd.type + " val1 is " + cmd.val1 + " val2 is " + cmd.val2);
+        }
+
+    }
+    void sendLoadDrop() {
+        if(mService.isTxPossible())
+        {
+            mService.writeRXCharacteristic(mParser.getLoadDropRequest());
+        }
+        else
+        {
+            ShowWarning("Nie można wysłać komendy!");
         }
     }
+
+    void sendGoHome() {
+        if(mService.isTxPossible())
+        {
+            mService.writeRXCharacteristic(mParser.getGoHomeRequest());
+        }
+        else
+        {
+            ShowWarning("Nie można wysłać komendy!");
+        }
+    }
+
+    void sendSetHome() {
+        if(mService.isTxPossible())
+        {
+            mService.writeRXCharacteristic(mParser.getSetHomeRequest());
+        }
+        else
+        {
+            ShowWarning("Nie można wysłać komendy!");
+        }
+    }
+
+
+
+    void onJoystickUpdate(int power, int angle) {
+        mJoyPower = power;
+        mJoyAngle = angle;
+    }
+
+    void periodicControlSend() {
+         byte[] controlVals = mParser.generateMotorValuesFromJoystick(mJoyPower, mJoyAngle);
+
+//         Log.d(TAG, "Control values: " + new String(controlVals, 0));
+         if (mService.isTxPossible()) {
+            mService.writeRXCharacteristic(controlVals);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/urmom/baitboatphoneapp/protocolParser.java
@@ -0,0 +1,259 @@
+package com.urmom.baitboatphoneapp;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class protocolParser {
+    private final static String TAG = "BaitBoatPhoneApp";
+
+    static final int TYPE_GPS = 0;
+    static final int TYPE_VOLTAGE = 1;
+    static final int TYPE_COMPASS = 2;
+    static final int TYPE_PRESSURE = 3;
+    static final int TYPE_TEMPERATURE = 4;
+    static final int TYPE_INVALID = 4;
+
+    static final int LONGEST_VALID_FRAME_SIZE = 19;
+
+
+    private int slowAngularVelThreshold = 45;
+    private double slowAngularCoeff = 0.4;
+    private int maxAllowedMotorVal = 100;
+    ArrayList<Byte> rxBuffer = new ArrayList<Byte>();
+
+    ConcurrentLinkedQueue<Command> rxParsedCommands = new ConcurrentLinkedQueue<Command>();
+
+    private class RxParseRule {
+        int type;
+        byte[] pattern;
+
+        RxParseRule(int t, byte[] p) {
+            type = t;
+            pattern = p;
+        }
+    }
+
+    public class Command {
+
+        int type;
+        double val1;
+        double val2;
+
+        public Command() {
+            type = TYPE_INVALID;
+            val1 = 0;
+            val2 = 0;
+        }
+    }
+
+    RxParseRule[] parsingRules = {new RxParseRule(TYPE_VOLTAGE, "ZXXXX_".getBytes()),
+            new RxParseRule(TYPE_GPS, "NXXXXXXXX-XXXXXXXX_".getBytes()),
+            new RxParseRule(TYPE_COMPASS, "MXX_".getBytes()),
+            new RxParseRule(TYPE_PRESSURE, "OXXXX_".getBytes()),
+            new RxParseRule(TYPE_TEMPERATURE, "PXXXX_".getBytes())};
+
+
+    // from joy: full up is 100 power, 0 angle, full left -90 angle, full right 90 angle, full back 180/-180 angle
+    byte[] generateMotorValuesFromJoystick(int power, int angle) {
+        double motorLeft = 0;
+        double motorRight = 0;
+        double linearVel = 1.0;
+        double angularVel = 1.0;
+
+        int motorRightInt = 0;
+        int motorLeftInt = 0;
+
+
+        // reverse mode math
+        if (Math.abs(angle) > 90) {
+            power = -power;
+
+            if (angle > 0) {
+                angle = 180 - angle;
+            } else {
+                angle = -180 - angle;
+            }
+        }
+
+
+        // mode for slow turning when going mostly forward/backward
+        if (Math.abs(angle) < slowAngularVelThreshold) {
+            angularVel = slowAngularCoeff;
+        }
+
+        // linear/angular
+        linearVel = ((90 - Math.abs(angle)) * power) / 90;
+        angularVel *= (double) angle * 10.0 / 9.0;
+
+
+        // values calc
+        motorLeft = linearVel + angularVel;
+        motorRight = linearVel - angularVel;
+
+        // Saturation check
+        double maxMotorVal = Math.max(Math.abs(motorLeft), Math.abs((motorRight)));
+        if (maxMotorVal > maxAllowedMotorVal) {
+            motorLeft *= ((double) maxAllowedMotorVal) / maxMotorVal;
+            motorRight *= ((double) maxAllowedMotorVal) / maxMotorVal;
+        }
+
+        motorLeftInt = (int) motorLeft;
+        motorRightInt = (int) motorRight;
+//        Log.d(TAG, "Motors L: " + motorLeftInt + " R: " + motorRightInt);
+        return encodeMotorValues(motorLeftInt, motorRightInt);
+    }
+
+    private byte[] encodeMotorValues(int left, int right) {
+        return String.format("G%02X%02X_", left + 128, right + 128).getBytes();
+    }
+
+    byte[] getLoadDropRequest() {
+        return "H_".getBytes();
+    }
+
+    byte[] getSetHomeRequest() {
+        return "SX_".getBytes();
+    }
+
+    byte[] getGoHomeRequest() {
+        return "XX_".getBytes();
+    }
+
+// "MXX_" - sends compass orientation in hex. value 0 is 0 degrees and 240 is 360 degrees. 241-255 invalid
+// "NXXXXXXXX-XXXXXXXX_" - sends GPS position in hex. first lattitude, second longitude. to parse back to double, divide both values by 1000000
+// "OXXXX_" - sends pressure in hex. to parse back to hPa, parse it as uint16, then divide by 10, at the end add 900.
+// "PXXXX_" - sends temperature in hex. to parse back to *C, parse it as uint16, then divide by 10, at the end add (-40).
+
+
+    void addByte(byte b) {
+        rxBuffer.add(new Byte(b));
+    }
+
+
+    void parseRxBuffer() {
+
+        boolean anyMatch = true;
+        while (anyMatch || (rxBuffer.size() > LONGEST_VALID_FRAME_SIZE)) {
+            anyMatch = false;
+
+            for (int i = 0; i < parsingRules.length; i++) {
+
+                if (isRxMatch(parsingRules[i])) {
+                    anyMatch = true;
+                    parseCommand(parsingRules[i].type);
+                    releaseRxBuffer(parsingRules[i].pattern.length);
+                }
+            }
+
+            // if no matches found and there is more bytes than longest command, something is wrong, remove one byte and try again
+            if ((anyMatch == false) && (rxBuffer.size() >= LONGEST_VALID_FRAME_SIZE)){
+                rxBuffer.remove(0);
+            }
+        }
+
+    }
+
+
+    boolean isRxMatch(RxParseRule rule) {
+        if (rxBuffer.size() < rule.pattern.length) {
+            return false;
+        }
+
+        for (int i = 0; i < rule.pattern.length; i++) {
+            switch (rule.pattern[i]) {
+                case 'X':
+                    if (false == isHexDigit(rxBuffer.get(i))) {
+                        return false;
+                    }
+                    break;
+                default:
+                    if (rule.pattern[i] != rxBuffer.get(i)) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return true;
+    }
+
+    boolean isHexDigit(byte digit) {
+        return (digit >= '0' && digit <= '9') || (digit >= 'A' && digit <= 'F');
+    }
+
+    long getValueOfHexDigit(byte digit) {
+        if (digit >= '0' && digit <= '9') {
+            return digit - '0';
+        } else {
+            return digit - 'A' + 10;
+        }
+    }
+
+    long parseHexInt(int startIdx, int size) {
+        long val = 0;
+        for (int i = 0; i < size; i++) {
+            val += Math.pow(16, size - i - 1) * getValueOfHexDigit(rxBuffer.get(startIdx + i));
+        }
+
+        return val;
+    }
+
+    void releaseRxBuffer(int numOfBytes) {
+        for (int i = 0; i < numOfBytes; i++) {
+            rxBuffer.remove(0);
+        }
+    }
+
+
+    void parseCommand(int commandType) {
+        Command cmd = new Command();
+        cmd.type = commandType;
+
+        switch (commandType) {
+            case TYPE_COMPASS:
+                cmd.val1 = parseHexInt(1, 2);
+                cmd.val1 *= 360.0 / 240.0;
+                break;
+            case TYPE_GPS:
+                cmd.val1 = parseHexInt(1, 8);
+                cmd.val2 = parseHexInt(10, 8);
+
+                cmd.val1 /= 1000000.0;
+                cmd.val2 /= 1000000.0;
+                break;
+            case TYPE_VOLTAGE:
+                cmd.val1 = parseHexInt(1, 4);
+                cmd.val1 /= 100.0;
+                break;
+
+            case TYPE_PRESSURE:
+                cmd.val1 = parseHexInt(1, 4);
+                cmd.val1 /= 10.0;
+                cmd.val1 += 900.0;
+                break;
+            case TYPE_TEMPERATURE:
+                cmd.val1 = parseHexInt(1, 4);
+                cmd.val1 /= 10.0;
+                cmd.val1 += -40.0;
+                break;
+            default:
+                break;
+        }
+
+        rxParsedCommands.add(cmd);
+    }
+
+
+    public boolean isRxCommandAvailable() {
+        return rxParsedCommands.size() > 0;
+    }
+
+    public Command getBoatMessage() {
+        return rxParsedCommands.poll();
+    }
+
+}
+
+
+
